68dfa785c28d1b12686c98055d0b322fa930f439
ck <chenkaisupervip@163.com>
2017-09-20 11:15:59 +0800

    增加注释

--- src/com/base/design/singleton/SingletonTest03.java
+++ src/com/base/design/singleton/SingletonTest03.java
@@ -5,8 +5,8 @@ import com.base.jdk8.People;
 /**
  * 单例模式的N中写法
  * 03：静态 内部类, 最安全的写法
- * 因为只有在调用内部类方法的时候才会去加载内部类，其他时候调用外部类是不会初始化内部类的，不占用空间，
- * 同时也线程安全；
+ * 因为只有在调用内部类方法的时候才会去加载内部类，初始化，实例化，并且只加载一次，其他时候调用外部类是不会初始化内部类的，
+ * 不占用空间，每个线程独一份，同时也线程安全；
  * <p>
  * Created by ck on 2017-09-18.
  */
@@ -16,13 +16,13 @@ public class SingletonTest03 {
 
         private static final People people = new People();
 
-        private static People GetPeople() {
-            return people;
+        private static People getPeople() {
+            return GeneratorPeople.people;
         }
     }
 
     public static void main(String[] args) {
-        SingletonTest03.GeneratorPeople.GetPeople();
+        SingletonTest03.GeneratorPeople.getPeople();
     }
 
 }
